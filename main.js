988be3df7fa4cd5f282f93c7fa7f4b76fa5cd6c2
antonioneco00 <antonionc2000@hotmail.es>
2021-04-16 13:24:03 +0200

    Subir

---
+++ main.js
@@ -0,0 +1,9 @@
+/// <reference path="jQuery/index.d.ts"/>
+
+$(document).ready(() => {
+    var botonEmpezar = $("#botonEmpezar");
+    var contenedorBotones = $("#contenedorBotones");
+    var botones = $(".botones");
+    botones.hide();
+    // contenedorBotones.css('display', 'grid');
+});
